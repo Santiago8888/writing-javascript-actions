df1fa9bd38aa55fef36f0b8479a1fdf76a430e08
Santiago8888 <santiago.marti67@gmail.com>
2020-06-27 03:21:51 -0500

    Fix octokit constructor.

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -6,8 +6,8 @@ async function run() {
       const issueTitle = core.getInput("issue-title");
       const jokeBody = core.getInput("joke");
       const token = core.getInput("repo-token");
-  
-      const octokit = new github.GitHub(token);
+
+      const octokit = github.getOctokit(token)
   
       await octokit.issues.create({
           repo: github.context.repo.repo,
